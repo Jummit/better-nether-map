45a5553ba46ca799b3632cf1eeaf6740d65aa862
Jummit <jummit@web.de>
2023-06-23 09:41:02 +0200

    Add a logger

--- src/main/java/com/jummit/nethermap/NetherMap.java
+++ src/main/java/com/jummit/nethermap/NetherMap.java
@@ -6,8 +6,12 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.Toml4jConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NetherMap implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("Better Nether Map");
+
 	@Override
 	public void onInitialize() {
 		UseItemCallback.EVENT.register(new UseItemCallbackListener());
